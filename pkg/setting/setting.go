6ff97475821667827fe6b62bcf4a5e2610340d9e
bianpeijiang <bianpeijiang@maiyatian.com>
2024-05-21 23:49:18 +0800

    update

--- pkg/setting/setting.go
+++ pkg/setting/setting.go
@@ -32,5 +32,24 @@ func init() {
 }
 
 func LoadBase() {
+	RunMode = Cfg.Section("").Key("RUN_MODE").MustString("debug")
+}
 
+func LoadServer() {
+	sec, err := Cfg.GetSection("server")
+	if err != nil {
+		log.Fatalf("Fail to get section 'server':%v", err)
+	}
+	HTTPPort = sec.Key("HTTP_PORT").MustInt(8000)
+	ReadTimeout = time.Duration(sec.Key("READ_TIMEOUT").MustInt(60)) * time.Second
+	WriteTimeout = time.Duration(sec.Key("WRITE_TIMEOUT").MustInt(60)) * time.Second
+}
+
+func LoadApp() {
+	sec, err := Cfg.GetSection("app")
+	if err != nil {
+		log.Fatalf("Fail to get section 'app': %v", err)
+	}
+	JwtSecret = sec.Key("JWT_SECRET").MustString("#(#)@@!@#!#!")
+	PageSize = sec.Key("PAGE_SIZE").MustInt(10)
 }
